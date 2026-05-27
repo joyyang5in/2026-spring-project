3f76cb0140d70a4c932bc54d8621275d47557b11
Will Chu <willchu.5in@gmail.com>
2026-05-27 15:50:42 +0800

    draw systems

--- students/will/scripts/graffiti.js
+++ students/will/scripts/graffiti.js
@@ -1,5 +1,68 @@
+/*
+ * HTML elements
+ */
 const graffitiCanvas = document.getElementById("my-graffiti");
-
-console.log(graffitiCanvas);
-
 const surface = graffitiCanvas.getContext("2d");
+const cleanButton = document.getElementById("clean");
+
+/*
+ * Graffiti style.
+ */
+surface.lineWidth = 13;
+surface.lineJoin = "round";
+surface.strokeStyle = "red";
+
+/*
+ * Shapes
+ */
+function shapes() {
+  surface.rect(150, 200, 100, 100);
+  surface.stroke();
+
+  surface.beginPath();
+  surface.moveTo(200, 50);
+  surface.lineTo(150, 200);
+  // Add a 7.
+  surface.moveTo(250, 50);
+  surface.lineTo(350, 50);
+  surface.moveTo(350, 50);
+  surface.lineTo(270, 300);
+
+  surface.closePath();
+  surface.stroke();
+}
+shapes();
+
+/*
+ * Clean up
+ */
+function cleanCanvas() {
+  surface.clearRect(0, 0, 400, 400);
+}
+cleanButton.addEventListener("click", cleanCanvas);
+
+/*
+*graffiti
+*/
+
+
+let oldX = 0;
+let oldY = 0;
+
+function graffiti(event){
+const x = event.offsetX;
+const y = event.offsetY;
+console.log(x, y, event.buttons);
+
+if (event.buttons === 1) {
+surface.beginPath();
+surface.moveTo(oldX, oldY);
+surface.lineTo(x, y);
+surface.closePath();
+surface.stroke();
+}
+
+oldX = x;
+oldY = y;
+}
+graffitiCanvas.addEventListener("mousemove", graffiti);
