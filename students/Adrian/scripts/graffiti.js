e54bec25643d9a59419e0291cf278ed57d7c4c82
Adrian <suadrian.5in@gmail.com>
2026-05-28 15:53:24 +0800

    cjange color and size

--- students/Adrian/scripts/graffiti.js
+++ students/Adrian/scripts/graffiti.js
@@ -4,35 +4,59 @@
 const graffitiCanvas = document.getElementById("my-graffiti");
 const surface = graffitiCanvas.getContext("2d");
 const cleanButton = document.getElementById("clean");
-
+const colorinput = document.getElementById("color-input");
+const sizeinput = document.getElementById("size-input");
 /*
 *Graffiti style
 */
 
-surface.lineWidth = 13;
-surface.lineJoin="round";
-surface.strokeStyle="red";
+surface.lineJoin = "round";
+console.log(colorinput);
+function changecolor() {
+    surface.strokeStyle = colorinput.value;
+}
+function changesize() {
+    surface.lineWidth = sizeinput.value;
+}
+changesize();
+sizeinput.addEventListener("change", changesize)
+changecolor();
+colorinput.addEventListener("change", changecolor)
+
+
+
 /*
 *shapes
  */
-function shapes(){
 
-
-surface.rect(150,200,100,100);
-surface.stroke();
-surface.moveTo(200,50);
-surface.lineTo(150,200);
-surface.closePath();
-surface.stroke();
-
-}
-shapes();
 /*
  * Clean
  */
-function cleanCanvas(){
-    surface.clearRect(0,0,400,400)
+function cleanCanvas() {
+    surface.clearRect(0, 0, 400, 400)
 
 }
 
-cleanButton.addEventListener("click",cleanCanvas);
+cleanButton.addEventListener("click", cleanCanvas);
+/**
+ * Graffiti
+ */
+let oldX = 0
+let oldY = 0
+function graffiti(event) {
+
+    const x = event.offsetX;
+    const y = event.offsetY;
+    console.log(x, y, event.buttons);
+
+    if (event.buttons === 1) {
+        surface.beginPath();
+        surface.moveTo(oldX, oldY);
+        surface.lineTo(x, y);
+        surface.closePath();
+        surface.stroke();
+    }
+    oldX = x;
+    oldY = y;
+}
+graffitiCanvas.addEventListener("mousemove", graffiti)
