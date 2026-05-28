5ade058399efd995803b85b42a936206b74840f3
Rex <rexlee.5in@gmail.com>
2026-05-28 15:52:29 +0800

    Graffitis labels

--- students/Rex/scripts/graffiti.js
+++ students/Rex/scripts/graffiti.js
@@ -4,44 +4,53 @@
 const graffitiCanvas = document.getElementById("my-graffiti");
 const surface = graffitiCanvas.getContext("2d");
 const cleanbutton = document.getElementById("clean");
-
+const colorInput = document.getElementById("color-input");
+const sizeInput = document.getElementById("size-input");
 
 /*
 graffiti style
 */
-
-surface.lineWidth = 13;
 surface.lineJoin = "round";
-surface.strokeStyle = "red";
+function changeColor() {
+    surface.strokeStyle = colorInput.value;
+};
+changeColor();
+colorInput.addEventListener("change", changeColor);
 
+function changeSize() {
+    console.log(sizeInput.value);
+    surface.lineWidth = sizeInput.value;
+}
+changeSize();
+sizeInput.addEventListener("change", changeSize);
 /*
 *shape
 */
 
-function shape(){
-surface.rect(100, 200, 100, 100);
-surface.stroke();
+function shape() {
+    surface.rect(100, 200, 100, 100);
+    surface.stroke();
 
-surface.beginPath();
-surface.moveTo(150,50);
-surface.lineTo(100,200);
-surface.closePath();
-surface.stroke();
+    surface.beginPath();
+    surface.moveTo(150, 50);
+    surface.lineTo(100, 200);
+    surface.closePath();
+    surface.stroke();
 
-surface.beginPath();
-surface.moveTo(225,50);
-surface.lineTo(230,150);
-surface.moveTo(220,50);
-surface.lineTo(325,50);
-surface.moveTo(325,50);
-surface.lineTo(310,300);
-surface.closePath();
-surface.stroke();
+    surface.beginPath();
+    surface.moveTo(225, 50);
+    surface.lineTo(230, 150);
+    surface.moveTo(220, 50);
+    surface.lineTo(325, 50);
+    surface.moveTo(325, 50);
+    surface.lineTo(310, 300);
+    surface.closePath();
+    surface.stroke();
 
 };
 shape();
 
-function cleanCanvas(){
+function cleanCanvas() {
     surface.clearRect(0, 0, 400, 400);
 };
 cleanbutton.addEventListener("click", cleanCanvas);
@@ -49,16 +58,16 @@ cleanbutton.addEventListener("click", cleanCanvas);
 let oldX = 0;
 let oldY = 0;
 
-function graffiti(event){
+function graffiti(event) {
     const x = event.offsetX;
     const y = event.offsetY;
     console.log(x, y, event.buttons);
-    if (event.buttons === 1){
-    surface.beginPath();
-    surface.moveTo(oldX, oldY);
-    surface.lineTo(x, y);
-    surface.closePath();
-    surface.stroke();
+    if (event.buttons === 1) {
+        surface.beginPath();
+        surface.moveTo(oldX, oldY);
+        surface.lineTo(x, y);
+        surface.closePath();
+        surface.stroke();
     };
     oldX = x;
     oldY = y;
