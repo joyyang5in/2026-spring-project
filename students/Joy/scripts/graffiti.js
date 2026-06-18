8cffd30d10f0466136c3d21f40fffb81acc4a170
Joy Y <joyyang.5in@gmail.com>
2026-06-18 15:56:23 +0800

    add mustache

--- students/Joy/scripts/graffiti.js
+++ students/Joy/scripts/graffiti.js
@@ -1,4 +1,9 @@
 import { CanvasEraser } from "../../../scripts/canvas-eraser.js";
+import { StickyClickyImage } from "../../../scripts/sticky-clicky-image.js";
+
+const stickyMustache = new StickyClickyImage('mustache');
+stickyMustache.setContainerID('dog-picture');
+
 /*
 *HTML elements
 */
